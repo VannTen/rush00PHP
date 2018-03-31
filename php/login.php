a7add9da9e9c17a9753d3fd96814bcb8cbf6eebb
Max Gautier <mgautier@student.42.fr>
2018-03-31 10:50:07 +0200

    check for session

--- php/login.php
+++ php/login.php
@@ -1,17 +1,17 @@
 <?php
 include __DIR__ . '/auth.php';
 function session_login($login, $passwd)
-	session_start();
-if (has_value('login') && has_value('passwd') && auth($_POST['login'], $_POST['passwd']))
+if (has_value('login') && has_value('passwd') && auth($_POST['login'], $_POST['passwd']) $$ isset($_SESSION))
 {
 	$account = get_user_account($_POST['login']);
 	$_SESSION['logged_on_user'] = $account['login'];
-	$_SESSION['group'] = $account['group']$
+	$_SESSION['group'] = $account['group']$;
 		echo "Successfully logged on\n";
 }
 else
 {
 	$_SESSION['logged_on_user'] = "";
+	$_SESSION['group'] = "";
 	echo "ERROR\n";
 }
 ?>
