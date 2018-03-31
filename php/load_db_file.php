f52a1accf519ea284218549620c3154b42fc5eeb
Max Gautier <mgautier@student.42.fr>
2018-03-31 14:48:09 +0200

    add readonly db

--- php/load_db_file.php
+++ php/load_db_file.php
@@ -1,15 +1,28 @@
 <?php
 function load_db_file($path)
 {
-	$file = fopen($path, "rw");
-	flock($passwd, LOCK_EX);
+	return (load_db_file_intern($path, 'rw'));
+}
+function load_db_file_intern($path, $mode)
+{
+	$file = fopen($path, $mode);
+	flock($file, LOCK_EX);
 	$data = unserialize(fread($file, filesize($path)));
-	$db = array('data' => $data, 'file', $file);
+	$db = array('data' => $data, 'file', $file, 'mode' => $mode);
 	return ($db);
 }
-
+function read_db_file($path)
+{
+	$file = fopen($path, 'r');
+	flock($file, LOCK_EX);
+	$data = unserialize(fread($file, filesize($path)));
+	flock($file, LOCK_UN);
+	fclose($file);
+	return ($data);
+}
 function commit_db_file($db)
 {
+	if ($db['mode'] == 'rw')
 	$data = serialize($db['data']);
 	fwrite($db['file'], $data);
 	flock($db['file'], LOCK_UN);
