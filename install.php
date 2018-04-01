b9e643e44e3812c7a09df254ad9df082ac9acd7b
Max Gautier <mgautier@student.42.fr>
2018-04-01 14:56:31 +0200

    fix install php

--- install.php
+++ install.php
@@ -1,13 +1,14 @@
 <?php
-include __DIR__ . '/auth.php';
+include __DIR__ . '/php/auth.php';
 $admin = array('login' => 'admin',
 	'passwd' => password_hash('admin', PASSWORD_DEFAULT),
 	'group' => 'admin');
 $user = array('login' => 'user',
 	'passwd' => password_hash('user', PASSWORD_DEFAULT),
 	'group' => 'active');
-reinit_account();
-commit_account($admin);
-commit_account($user);
+unlink('bdd/passwd');
+$accounts = array($admin, $user);
+$db = array('data' => $accounts, 'path' => 'bdd/passwd');
+commit_db_file($db);
 echo "Admin account has been created with default password\n";
 ?>
