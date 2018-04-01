06e8a583d6301f3b817c45ccb8de27b24740d04d
Max Gautier <mgautier@student.42.fr>
2018-04-01 21:29:59 +0200

    config files

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker-machine create --driver 'virtualbox' dev-php
+docker-machine start dev-php
+eval $(docker-machine env dev-php)
+sed -i '' -e "s/SERVER_NAME/`docker-machine env dev-php`" conf/rush.conf
+docker run -d -p 80:80 \
+	-v $PWD/conf/rush.conf:/etc/apache2/sites-available/000-default.conf \
+	-v $PWD:/var/www/html --name rush00 akeneo/apache-php
+docker start rush00
+curl http://`docker-machine ip dev-php`/install.php
